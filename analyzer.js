7cd2c0a7d3c5d205488d4c6623fc9e6009e37278
rajashekar <rajashekarkhallel@gmail.com>
2018-10-05 17:35:48 +0530

    added word and charecter counts

--- analyzer.js
+++ analyzer.js
@@ -5,26 +5,26 @@
     var arr=[]; //array to hold all
     var baseChar=97; //char code for a
     var t1="";
-    
-    
+
+
     //create array
     for(var i=0;i<26;i++){
             arr[i]=new Object({char:String.fromCharCode(baseChar+i) ,frquency:0});
         }
-    
+
 
     while(counter>=0)
         {
             t1= text[counter].charCodeAt(0)-baseChar; 
-            
+
             if(t1<0 || t1>25) // neglect other than alphabets
                 ;
             else
                 arr[t1].frquency=arr[t1].frquency+1;
-            
+
             counter--;
         }
-    
+
 }
 */
 
@@ -36,7 +36,7 @@ $(function(){
 
     $("#cb_OnlyAlpha").checkbox("check");
     $("#cb_CombineCase").checkbox("check");
-    
+
     updateConfiguration();
 })
 
@@ -51,32 +51,36 @@ function updateConfiguration () {
     }
 }
 function analyze (e) {
-    
+
     //check if text entered 
     if(document.getElementById("txtData").value==""){
         alert("place your text and click on analyze (:)");
         return;
     }
-    
+
     cleanOld();
-    
+
     var data=$("#txtData").val();
     var words=extractWords(data);
     var chars=extractCharecters(data);
 
+    //bind counts to labels
+    $("#spnTotalWordsCount").html(words.totalWCount);
+    $("#spnTotalCharsCount").html(chars.totalCharCount);
+
     //convert to arrays
     var charsArray=[];
-    for(var i in chars){
-        charsArray.push(chars[i]);
+    for(var i in chars.charFreq){
+        charsArray.push(chars.charFreq[i]);
     }
 
     window.analyzer.data={
-        words:words,
+        words:words.wordsFreq,
         chars:charsArray
     };
 
     //binding functions
-    bind_wordCount(words);
+    bind_wordCount(words.wordsFreq);
 
     bind_CharecterCount(charsArray);
     draw_CharecterFrequency(charsArray);
@@ -86,54 +90,67 @@ function analyze (e) {
 
 //:>> word analysis
 function extractWords (text) {
-        //:>> words (separated by space or . or by ,)
+    //:>> words (separated by space or . or by ,)
     var words=text.split(" ").join(",")
-                  .split(".").join(",")
-                  .split(",");
+    .split(".").join(",")
+    .split(",");
 
     var wordObj= {};//[words.length];
+    var totalWCount=words.length;
 
     for(var i=0;i<words.length;i++){
-        
+
         var cw=words[i].toLowerCase();
 
-//:>> exclude spaces
-    if(cw.trim()=="")
-        continue;
+        //:>> exclude spaces
+        if(cw.trim()==""){
+            totalWCount=totalWCount-1;
+            continue;
+        }
 
-//:>>if word already exists increment count else create new 
+
+        //:>>if word already exists increment count else create new 
         if(wordObj[cw])
             wordObj[cw].count=wordObj[cw].count+1;
         else
             wordObj[cw]={ value:cw,count:1 }
     }
 
-     return wordObj;             
+    return {
+        totalWCount:totalWCount,
+        wordsFreq:wordObj
+    }; 
 }
 
 function extractCharecters (text) {
-    
+
     //eliminate non alphabets
-    var charObj={};
-   for(var i=0;i<text.length-1;i++){
-    
-    var ch=text[i].toLowerCase();
+    var charObj={},totalCharCount=0;
+    for(var i=0;i<=text.length-1;i++){
 
-    //:>> exclude spaces
-    if(ch.trim()=="")
-        continue;
-    
-    //apply rules
-    if(window.analyzer.waConfig.onlyAlpha)
-        if(!isAlpha(ch))continue;
+        var ch=text[i].toLowerCase();
 
-     if(charObj[ch])
+
+        //:>> exclude spaces
+        if(ch.trim()=="")
+            continue;
+
+
+        //apply rules
+        if(window.analyzer.waConfig.onlyAlpha)
+            if(!isAlpha(ch))continue;
+
+        totalCharCount++;
+        if(charObj[ch])
             charObj[ch].count=charObj[ch].count+1;
         else
             charObj[ch]={ value:ch,count:1 }
-   }
+    }
 
-   return charObj;
+    return {
+        totalCharCount:totalCharCount,
+        charFreq:charObj
+    };
 }
 
 //:>>binding functions==> data binding functions
@@ -142,11 +159,11 @@ function bind_wordCount(data){
     var html="";
 
     for(var i in data){
-      
-      html=html+ '<div class="ui label">'
-                    + data[i].value 
-                    + '<div class="detail">'+ data[i].count 
-                    + '</div></div>'
+
+        html=html+ '<div class="ui label">'
+            + data[i].value 
+            + '<div class="detail">'+ data[i].count 
+            + '</div></div>'
 
         html=html.trim();
     }
@@ -163,11 +180,11 @@ function bind_CharecterCount (data) {
     var html="";
 
     for(var i=0;i<data.length;i++){
-      
-      html=html+ '<a class="ui label">'
-                    + data[i].value 
-                    + '<div class="detail">'+ data[i].count 
-                    + '</div> </a>'
+
+        html=html+ '<a class="ui label" onclick="showSelectChar(this)">'
+            + data[i].value 
+            + '<div class="detail">'+ data[i].count 
+            + '</div> </a>'
 
         html=html.trim();
     }
@@ -179,7 +196,7 @@ function bind_CharecterCount (data) {
 }
 
 function draw_CharecterFrequency (data) {
-    
+
     var graphData=[{
         key:"charecter frequency",
         values:data
@@ -198,8 +215,8 @@ function draw_CharecterFrequency (data) {
         chart.yAxis.tickFormat(d3.format(',f'));
 
         d3.select('#divCharFreqGraph svg')
-        .datum(graphData)
-        .call(chart);
+            .datum(graphData)
+            .call(chart);
 
         nv.utils.windowResize(chart.update);
 
@@ -208,11 +225,16 @@ function draw_CharecterFrequency (data) {
 }
 
 
+function showSelectChar (el) {
+    debugger;
+
+    $(el).popup('show')
+}
 //:>> --------------------------- utils function  ------------------------------------------
 
 //return weather the charecter is an alphbet or not
 function isAlpha (ch) {
-    
+
     var isChar=false;
     var charcode;
 
